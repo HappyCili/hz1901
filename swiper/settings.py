d5cfcafd13479ad0e9b30465e656be0616a5193e
HappyCili <471109931@qq.com>
2019-12-24 16:35:29 +0800

    添加了模型

--- swiper/settings.py
+++ swiper/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.staticfiles',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -72,8 +73,12 @@ WSGI_APPLICATION = 'swiper.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "jy",
+        "HOST": "106.12.75.73",
+        "USER": "root",
+        "PASSWORD": "wzt521921",
+        "PORT": 3306,
     }
 }
 
--- user/models.py
+++ user/models.py
@@ -1,3 +1,26 @@
+import datetime
+
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    SEX = (
+        ('male', "男性"),
+        ('femal', "女性"),
+    )
+    LOCATION = (
+        ('北京', '北京'),
+        ('上海', '上海'),
+        ('广州', '广州'),
+        ('深圳', '深圳'),
+        ('杭州', '杭州'),
+        ('武汉', '武汉'),
+        ('成都', '成都'),
+    )
+    phonenum = models.CharField(max_length=16, unique=True, verbose_name="手机号")
+    nickname = models.CharField(max_length=16, verbose_name="昵称")
+    sex = models.CharField(max_length=8, choices=SEX, verbose_name="性别")
+    birthday = models.Field(default=datetime.date(1990,1,1), verbose_name="出生日")
+    avatar = models.CharField(max_length=256, verbose_name="个人形象")
+    location  = models.CharField(max_length=16, choices=LOCATION, verbose_name="常居地")
