0c301f6788a913c970eff78b8d6e6c68dba0410a
HappyCili <471109931@qq.com>
2019-12-25 10:50:32 +0800

    '封装http接口'

--- user/apis.py
+++ user/apis.py
@@ -6,15 +6,15 @@ from django.core.cache import cache
 from swiper.user.models import User
 from swiper.user import logics
 from swiper.common import stat, keys
-
+from swiper.lib.http import render_json
 
 def get_vcode(request):
     ''' 用户获取验证码'''
     phonenum = request.GET.get("phonenum")
     if logics.send_vcode(phonenum):
-        return JsonResponse({'code': stat.OK, "data": None})
+        return render_json(code=stat.OK, data=None)
     else:
-        return JsonResponse({'code': stat.SMSErr, "data": "SMSError"})
+        return render_json(code=stat.SMSErr, data="SMSError")
 
 def check_vcode(request):
     '''检查验证码， 并进行登录注册'''
@@ -23,7 +23,7 @@ def check_vcode(request):
     cache_vcode = cache.get(keys.VCODE_KEY % phonenum)
     # 检测验证码是否过期
     if cache_vcode is None:
-        return JsonResponse({'code': stat.VcodeExpired, "data": "VcodeExpired"})
+        return render_json(code=stat.VcodeExpired, data="VcodeExpired")
     # 判断验证码是否一致
     if cache_vcode == vcode:
         try:
@@ -32,6 +32,6 @@ def check_vcode(request):
             user = User.objects.create(phonenum=phonenum, nickname=phonenum)
         # 使用session记录登录状态
         request.session['uid'] = user.id
-        return JsonResponse({'code': 0, 'data': user.to_dict()})
+        return render_json(code=0, data=user.to_dict())
     else:
-        return JsonResponse({'code': stat.VcodeErr, "data": "VcodeError"})
+        return render_json(code=stat.VcodeErr, data="VcodeError")
